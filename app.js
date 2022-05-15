8e379af6bf1c638b04188ba7baddf2ddac118773
baritone46 <85428048+baritone46@users.noreply.github.com>
2022-05-15 20:41:56 +0900

    20220515ver

---
+++ app.js
@@ -0,0 +1,33 @@
+var alph = "abcdefghijklmnopqrstuvwxyz";
+var $mondai = document.getElementById('mondai');
+var $kaitou = document.getElementById('kaitou');
+var $mondai = document.getElementById('mondai');
+var $hantei = document.getElementById('hantei');
+var num = 0
+
+document.addEventListener('keypress',keypress_ivent);
+
+function keypress_ivent(e) {
+    $kaitou.innerHTML = e.key;
+
+    if($kaitou.innerHTML === $mondai.innerHTML){
+        $hantei.classList.remove('ng');
+        $hantei.classList.add('ok');
+        $hantei.innerHTML ="○正解！";
+        // window.alert('正解！');
+        // score++;
+        function getRandam(n, m){
+              num = Math.floor(Math.random() * (m + 1 - n)) + n;
+              console.log(num);
+          };
+        getRandam(0, 26);
+        $mondai.innerHTML = alph.substring(num,num + 1);
+       } else {
+        $hantei.classList.remove('ok');
+        $hantei.classList.add('ng');
+        $hantei.innerHTML ="×不正解！";
+        // window.alert('不正解！');
+       }
+
+    return false;
+}
